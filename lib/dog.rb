566eace75ac5eb1f5ae721162b73295e89097d8d
Jerome <jeromeallen99@gmail.com>
2023-05-13 15:58:09 -0400

    commit tree c38928999eb0723637c1a68b6c5300feab5c8de8

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
+    # setter method
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
 
+    # getter method
+    def name
+        @this_dogs_name
+    end
 end
+
+#this_dogs_name is a local variable
+# we need an instance vairable - @
